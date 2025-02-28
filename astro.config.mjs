cb9aa52c50a782eeaa68fdef1d904c8c342a2958
ofins <jkyeewang@gmail.com>
2025-02-28 16:19:57 +0800

    dist

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from "astro/config";
-
+// import staticAdapter from "@astrojs/static";
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+  integrations: [tailwind()],
+  base: "/ofins.github.io/",
+  output: "static",
+  // adapter: staticAdapter(),
 });
